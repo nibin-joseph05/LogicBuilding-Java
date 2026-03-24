8e2f4ae7d6c16850d0f488474b9400b09fce4a95
Nibin Joseph <nibin.joseph.career@gmail.com>
2026-03-24 12:43:19 +0530

    day01 commit

---
+++ LogicBuilding-Java-Nibin/src/LogicBuildingMain.java
@@ -0,0 +1,16 @@
+//main class
+
+import java.util.Scanner;
+
+import day01.Day01Programs;
+
+public class LogicBuildingMain{
+    public static void main(String[] args){
+        //input
+        Scanner scanner = new Scanner(System.in);
+        //day01
+        Day01Programs day01 = new Day01Programs();
+        day01.printNumberEnteredByUser(scanner);
+
+    }
+}
