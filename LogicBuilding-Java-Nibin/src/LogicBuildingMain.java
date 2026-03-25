a9ccf4ab34f43b36a29e10f1fe93848f595a81a2
Nibin Joseph <nibin.joseph.career@gmail.com>
2026-03-25 15:18:59 +0530

    day03 commit

--- LogicBuilding-Java-Nibin/src/LogicBuildingMain.java
+++ LogicBuilding-Java-Nibin/src/LogicBuildingMain.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 import day01.Day01Programs;
 import day02.Day02Programs;
+import day03.Day03Programs;
 
 public class LogicBuildingMain{
     public static void main(String[] args){
@@ -14,8 +15,12 @@ public class LogicBuildingMain{
 //        day01.run(scanner);
 
         //day02
-        Day02Programs day02 = new Day02Programs();
-        day02.run(scanner);
+//        Day02Programs day02 = new Day02Programs();
+//        day02.run(scanner);
+
+        //day03
+        Day03Programs day03 = new Day03Programs();
+        day03.run(scanner);
 
 
         scanner.close();
---
+++ LogicBuilding-Java-Nibin/src/day03/Day03Programs.java
@@ -0,0 +1,76 @@
+/*
+DAY - 03
+Program to Calculate the Square and Cube of a Number:
+
+Write a program where the user enters a number, and the program calculates its square and cube. For example:
+
+Input: Enter a number: 3
+Output: Square: 9, Cube: 27
+Program to Calculate the Area of a Circle and Triangle:
+
+Write a program to calculate the area of a circle given its radius and a triangle given its base and height. For example:
+
+Input: Radius: 5, Base: 10, Height: 4
+Output:
+Area of Circle: 78.5
+Area of Triangle: 20
+Write a Program to Find the Quotient and Remainder of Two Integers:
+
+Write a program where the user enters two integers (divisor and dividend) and calculates their quotient and remainder. For example:
+
+Input: Dividend: 22, Divisor: 7
+Output:
+Quotient: 3
+Remainder: 1
+*/
+
+package day03;
+import java.util.Scanner;
+
+public class Day03Programs {
+    public String squareAndCubeOfANumber(double num){
+        double square = num * num ;
+        double cube = num * num * num;
+        return "square : " + square + " cube : " + cube  ;
+    }
+
+    public String areaOfCircleAndTriangle(double radius, double base, double height) {
+        double circleArea = Math.PI * radius * radius;
+        double triangleArea = base * height / 2;
+        return "\nArea of Circle: " + circleArea  +
+               "\nArea of Triangle: " + triangleArea ;
+    }
+
+    public String quotientAndRemainderOfTwoIntegers(double dividend, double divisor){
+        double quotient = dividend / divisor;
+        double remainder = dividend % divisor;
+        return "Quotient : " + quotient + "Remainder : " + remainder;
+    }
+
+    public void run(Scanner scanner){
+
+        System.out.println("Enter a Number : ");
+        double num = scanner.nextDouble();
+        System.out.println(squareAndCubeOfANumber(num));
+
+        System.out.println("Enter the radius : ");
+        double radius = scanner.nextDouble();
+
+        System.out.println("Enter the base : ");
+        double base = scanner.nextDouble();
+
+        System.out.println("Enter the height : ");
+        double height = scanner.nextDouble();
+
+        System.out.println(areaOfCircleAndTriangle(radius, base, height));
+
+        System.out.println("Enter the Dividend : ");
+        double dividend = scanner.nextDouble();
+
+        System.out.println("Enter the Divisor : ");
+        double divisor = scanner.nextDouble();
+
+        System.out.println(quotientAndRemainderOfTwoIntegers(dividend, divisor));
+
+    }
+}
